a92fe746522977950f6ada13e36f8204faf6e226
Morten Dahl <douglas.botley@gmail.com>
2014-01-20 18:46:24 +0100

    heap_sort

--- basic/sorting.py
+++ basic/sorting.py
@@ -281,4 +281,126 @@ def closest_match(numbers, target):
 			return neighbourIndex
 		else:
 			return index
+
+
+
+
+
+
+
+#
+# heap structure, for min-heaps and max-heaps
+#
+class Heap:
+	
+	def __init__(self, heap=[]):
+		self._heap = heap
+		self._heapsize = len(heap)
+		
+	def value(self, i):
+		return _heap[i]
+		
+	def parent(self, i):
+		return (i-1) // 2
+
+	def left(self, i):
+		return 2*i + 1
+
+	def right(self, i):
+		return 2*i + 2
+
+	def print_list(self):
+		print self._heap
+
+	def print_tree(self, i=0, indent="", step="  "):
+		if not i < self._heapsize: return
+		print "{0}{1} [{2}]".format(indent, self._heap[i], i)
+		self.print_tree(self.left(i), indent+step)
+		self.print_tree(self.right(i), indent+step)
+		
+	# assuming heaps at left(i) and right(i) are max-heaps, turns heap at i into max-heap
+	#  - worst-case running time is O(h) where h = ln n is height of tree rooted at i
+	def max_heapify(self, i):
+		print "max heapify"
+		largest = i
+		l = self.left(i)
+		if l < self._heapsize and self._heap[l] > self._heap[i]: largest = l
+		r = self.right(i)
+		if r < self._heapsize and self._heap[r] > self._heap[largest]: largest = r
+		if largest != i:
+			# swap trick from stack overflow
+			#  - supposedly works because Python first evaluates all of the right side
+			self._heap[i], self._heap[largest] = self._heap[largest], self._heap[i]
+			self.max_heapify(largest)
+	
+	# assuming heaps at left(i) and right(i) are min-heaps, turns heap at i into min-heap
+	#  - worst-case running time is O(h) where h = ln n is height of tree rooted at i
+	def min_heapify(self, i):
+		smallest = i
+		l = self.left(i)
+		if l < self._heapsize and self._heap[l] < self._heap[i]: smallest = l
+		r = self.right(i)
+		if r < self._heapsize and self._heap[r] < self._heap[smallest]: smallest = r
+		if smallest != i:
+			# swap trick from stack overflow
+			#  - supposedly works because Python first evaluates all of the right side
+			self._heap[i], self._heap[smallest] = self._heap[smallest], self._heap[i]
+			self.min_heapify(smallest)
+	
+	# transforms array into max-heap
+	#  - worst-case running time is O(n)
+	def build_max_heap(self):
+		arraysize = len(self._heap)
+		self._heapsize = arraysize
+		# the last parent must be at index i mid-way in the array since left(i)/right(i) would overflow
+		lastparent = (arraysize // 2) - 1
+		# loop from 'lastparent' down to 0
+		for i in xrange(lastparent, -1, -1):
+			self.max_heapify(i)
+
+	# transforms array into min-heap
+	#  - worst-case running time is O(n)
+	def build_min_heap(self):
+		arraysize = len(self._heap)
+		self._heapsize = arraysize
+		# the last parent must be at index i mid-way in the array since left(i)/right(i) would overflow
+		lastparent = (arraysize // 2) - 1
+		# loop from 'lastparent' down to 0
+		for i in xrange(lastparent, -1, -1):
+			self.min_heapify(i)
+			
+	def sort_max(self):
+		self.build_max_heap()
+		for i in xrange(len(self._heap)-1, 0, -1):
+			# swap trick from stack overflow
+			#  - supposedly works because Python first evaluates all of the right side
+			self._heap[0], self._heap[i] = self._heap[i], self._heap[0]
+			self._heapsize -= 1
+			self.max_heapify(0)
+			self.print_list()
+			
+	def sort_min(self):
+		self.build_min_heap()
+		# smallest element at root, so 
+		for i in xrange(len(self._heap)-1, 0, -1):
+			# swap trick from stack overflow
+			#  - supposedly works because Python first evaluates all of the right side
+			self._heap[0], self._heap[i] = self._heap[i], self._heap[0]
+			self._heapsize -= 1
+			self.min_heapify(0)
+
+
+#
+# best case: O(n * lg n)
+#
+# worst case: O(n * lg n)
+#
+# average case: O(n * lg n)
+#
+# amortised: only invoked once
+#
+def heap_sort(list_to_sort):
+	heap = Heap(list_to_sort)
+	heap.sort_max()
+
 	
