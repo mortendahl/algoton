0c18ecfa9739f91756dc291bee6c5c9028d84a43
Morten Dahl <douglas.botley@gmail.com>
2014-02-25 14:51:17 +0100

    word split

--- dynamic-greedy.py
+++ dynamic-greedy.py
@@ -82,14 +82,14 @@ t0 = [2, 3, 1, 3, 4]
 t1 = [2, 1, 2, 2, 1]
 
 # naive approach
-path_same, price_same   = assembly_line_scheduling_naive(a0, a1, t0, t1, 0, 1)
-path_other, price_other = assembly_line_scheduling_naive(a1, a0, t1, t0, 1, 0)
-if price_same <= price_other: print path_same 
-else: print path_other
+#path_same, price_same   = assembly_line_scheduling_naive(a0, a1, t0, t1, 0, 1)
+#path_other, price_other = assembly_line_scheduling_naive(a1, a0, t1, t0, 1, 0)
+#if price_same <= price_other: print path_same 
+#else: print path_other
 
 # dynamic programming approach
-f0, f1, l0, l1 = assembly_line_scheduling_dynamic(a0, a1, t0, t1)
-print assembly_line_scheduling_dynamic_extract_way(l0, l1, argmin(f0[-1], f1[-1]))
+#f0, f1, l0, l1 = assembly_line_scheduling_dynamic(a0, a1, t0, t1)
+#print assembly_line_scheduling_dynamic_extract_way(l0, l1, argmin(f0[-1], f1[-1]))
 
 
 
@@ -108,8 +108,56 @@ print assembly_line_scheduling_dynamic_extract_way(l0, l1, argmin(f0[-1], f1[-1]
 #
 #  Word split
 #
-#  - dynamic programming algorithm
+#  - dynamic programming
 #
+
+def word_split(str_to_split, words):
+    # initialise array storing if it's possible to split sub-string str_to_split[:i+1]
+    split_is_possible = [False]*len(str_to_split)
+    # initialise array storing the word that made a split possible (or None if not)
+    split_word = [None]*len(str_to_split)
+    # bottom-up solving of subproblems
+    for i in xrange(0, len(str_to_split)):
+        # consider whole sub-word
+        whole_word = str_to_split[:i+1]
+        if whole_word in words: 
+            # a split is possible if it is a word by itself
+            split_is_possible[i] = True
+            split_word[i] = whole_word
+        else:
+            # .. and if it is not a word by itself, try its subsub-words
+            for j in range(i,0,-1):
+                #left = str_to_split[0:j]
+                right = str_to_split[j:i+1]
+                if right in words and split_is_possible[j-1]: 
+                    split_word[i] = right
+                    split_is_possible[i] = True
+    # test if it was possible to spil all the way up to the entire string
+    if split_is_possible[-1]:
+        # a split of the entire string is possible; extract words than made it possible
+        str_words = []
+        i = len(str_to_split)-1
+        while i >= 0:
+            cur = split_word[i]
+            str_words.append(cur)
+            i -= len(cur)
+        str_words.reverse()
+        return str_words
+    else:
+        # it was not possible to split the entire string
+        return None
+        
+
+print word_split("carrothouse", set(["car", "carrot", "house"]))
+print word_split("carrothouses", set(["car", "carrot", "house"]))
+print word_split("stringintowords", set(["string", "ring", "in", "to", "into", "words"]))
+print word_split("finestring", set(["fine", "ring", "string"]))
+print word_split("iseeyourattachment", set(["i", "see", "you", "your", "rat", "at", "attachment"]))
+
+
+
+
+
 
 
 
